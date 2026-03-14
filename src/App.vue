165900c3ae44b36a50d70bdb755e0a40e57cfbac
lenaanderson5566-ops <lenaanderson5566@gmail.com>
2026-03-15 06:09:07 +0800

    style: move card line-height rules into global assets styles

--- src/App.vue
+++ src/App.vue
@@ -487,14 +487,6 @@ export default {
 }
 
 
-.dashboard-card p,
-.section-wrapper .info-label,
-.section-wrapper .info-value,
-.section-wrapper .summary-label,
-.section-wrapper .summary-value {
-  line-height: 1.35;
-}
-
 .content-layout-shell {
   width: 100%;
   max-width: 1280px;
