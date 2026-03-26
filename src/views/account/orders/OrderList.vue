0543e6576ff1501b0087dd6ca0c17eb5243f18e6
lenaanderson5566-ops <lenaanderson5566@gmail.com>
2026-03-26 14:54:58 +0800

    fix(orders): show date only in created time column

--- src/views/account/orders/OrderList.vue
+++ src/views/account/orders/OrderList.vue
@@ -177,9 +177,7 @@ const formatDate = (timestamp) => {
   return date.toLocaleString(locale.value === 'zh-CN' ? 'zh-CN' : 'en-US', {
     year: 'numeric',
     month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit'
+    day: '2-digit'
   });
 };
 
