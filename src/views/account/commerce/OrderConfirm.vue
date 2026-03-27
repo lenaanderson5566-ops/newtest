d945594fb012de8939d941f168871227fd006c48
lenaanderson5566-ops <lenaanderson5566@gmail.com>
2026-03-27 18:15:25 +0800

    fix(order-confirm): remove mobile local edge compensation

--- src/views/account/commerce/OrderConfirm.vue
+++ src/views/account/commerce/OrderConfirm.vue
@@ -2676,9 +2676,6 @@ export default {
 @media (max-width: 768px) {
   .order-confirm-container {
     margin-top: 15px;
-    width: calc(100% + 4px);
-    margin-left: -2px;
-    margin-right: -2px;
 
     :deep(.page-inner) {
       padding-left: 0 !important;
