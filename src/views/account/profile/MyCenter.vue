bca05eaffac1cdbb6a3651466534cef95acd2886
lenaanderson5566-ops <lenaanderson5566@gmail.com>
2026-03-29 09:15:42 +0800

    fix(my-center): 最近登录标题移到卡片外并使用小标题样式

--- src/views/account/profile/MyCenter.vue
+++ src/views/account/profile/MyCenter.vue
@@ -47,10 +47,8 @@
           </div>
         </section>
 
+        <p class="section-subtitle">{{ $t('myCenter.recentLoginTitle') }}</p>
         <section class="section-block dashboard-like-card recent-login-panel">
-          <div class="recent-login-header">
-            <div class="row-title">{{ $t('myCenter.recentLoginTitle') }}</div>
-          </div>
           <div class="recent-login-list">
             <div v-if="recentLoginLoading" class="recent-login-state">{{ $t('myCenter.loadingRecentLogin') }}</div>
             <div v-else-if="!recentLoginRecords.length" class="recent-login-state">{{ $t('myCenter.recentLoginEmpty') }}</div>
@@ -651,7 +649,7 @@ $space-2: map.get($spacers, 2);
 }
 
 .recent-login-list {
-  margin-top: 12px;
+  margin-top: 0;
   border-top: 1px solid rgba(var(--text-color-rgb), 0.08);
 }
 
