34c523d871cac87016e1385c537f7c52cd32c053
lenaanderson5566-ops <lenaanderson5566@gmail.com>
2026-03-30 10:06:20 +0800

    fix(dashboard): match today traffic number weight with quota cards

--- src/views/overview/Dashboard.vue
+++ src/views/overview/Dashboard.vue
@@ -2098,9 +2098,11 @@ $space-2: map.get($spacers, 2);
       .usage-percent {
         line-height: 1;
         font-size: $font-size-xl;
+        font-weight: $font-weight-bold;
 
         &.compact {
           font-size: $font-size-xl;
+          font-weight: $font-weight-bold;
         }
       }
 
