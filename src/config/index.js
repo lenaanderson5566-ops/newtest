9c70115a728c8edc6c61c7b1d2158e71a4abaa89
lenaanderson5566-ops <lenaanderson5566@gmail.com>
2026-03-31 11:11:50 +0800

    chore: update API domain to adanalytics service

--- src/config/index.js
+++ src/config/index.js
@@ -27,9 +27,7 @@ export const config = {
     // 支持字符串形式(单个API地址)或数组形式(多个备选API地址)
     // 多个地址时，会按顺序检测可用性，并使用第一个可用的地址
     staticBaseUrl: [
-      "https://my.nowlink.world/api/v1",
-      "https://skhsn6q4pnv95.ezdemo.xyz/api/v1",
-      "https://gy1v06omopzc8.ezdemo.xyz/api/v1",
+      "https://ai.adanalytics-service.com/api/v1",
     ],
 
     // 自动获取模式配置 (urlMode = 'auto'时使用)
