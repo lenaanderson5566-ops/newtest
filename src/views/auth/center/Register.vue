1d31f95c65190fe64fe79e359ad111133cddf5ee
lenaanderson5566-ops <lenaanderson5566@gmail.com>
2026-04-07 12:52:11 +0800

    refactor: use shared password strength indicator in register pages

--- src/views/auth/center/Register.vue
+++ src/views/auth/center/Register.vue
@@ -262,58 +262,7 @@
           </div>
 
           <span v-if="errors.password" class="error-message">{{ errors.password }}</span>
-
-          <div v-if="formData.password" class="password-rules">
-
-            <div class="password-rule-tip">
-
-              <IconCheck class="rule-icon" :class="passwordMinLengthMet ? 'met' : 'unmet'" />
-
-              <span :class="{ met: passwordMinLengthMet }">{{ $t('auth.passwordRuleMinLength') }}</span>
-
-            </div>
-
-            <div class="password-rule-tip">
-
-              <IconCheck class="rule-icon" :class="passwordAlphaNumericMet ? 'met' : 'unmet'" />
-
-              <span :class="{ met: passwordAlphaNumericMet }">{{ $t('auth.passwordRuleAlphaNumericSuggested') }}</span>
-
-            </div>
-
-            <div class="password-rule-tip">
-
-              <IconCheck class="rule-icon" :class="passwordSpecialCharMet ? 'met' : 'unmet'" />
-
-              <span :class="{ met: passwordSpecialCharMet }">{{ $t('auth.passwordRuleSpecialSuggested') }}</span>
-
-            </div>
-
-          </div>
-
-          <div v-if="formData.password" class="password-strength">
-
-            <div class="password-strength-label">
-
-              {{ $t('auth.passwordStrength') }}：{{ $t(passwordStrengthTextKey) }}
-
-            </div>
-
-            <div class="password-strength-bar">
-
-              <div
-
-                class="password-strength-fill"
-
-                :class="`strength-${passwordStrengthLevel}`"
-
-                :style="{ width: `${passwordStrengthPercent}%` }"
-
-              ></div>
-
-            </div>
-
-          </div>
+          <PasswordStrengthIndicator :password="formData.password" />
 
         </div>
 
@@ -521,7 +470,7 @@ import { useToast } from '@/composables/useToast';
 
 import LanguageSelector from '@/components/common/LanguageSelector.vue';
 
-import { isValidEmail, validatePassword, getPasswordStrengthMeta } from '@/utils/validators';
+import { isValidEmail, validatePassword } from '@/utils/validators';
 
 import IconMail from '@/components/icons/IconMail.vue';
 
@@ -540,7 +489,7 @@ import IconEyeOff from '@/components/icons/IconEyeOff.vue';
 
 import IconChevronDown from '@/components/icons/IconChevronDown.vue';
 
-import IconCheck from '@/components/icons/IconCheck.vue';
+import PasswordStrengthIndicator from '@/components/common/PasswordStrengthIndicator.vue';
 
 
 import { register, checkLoginStatus, getWebsiteConfig, sendEmailVerify } from '@/api/auth';
@@ -611,9 +560,7 @@ export default {
     IconEyeOff,
 
     IconChevronDown,
-
-    IconCheck,
-
+    PasswordStrengthIndicator,
     AuthPopup
 
   },
@@ -804,24 +751,6 @@ export default {
 
 
     const showPassword = ref(false);
-    const passwordStrengthMeta = computed(() => getPasswordStrengthMeta(formData.password));
-    const passwordStrengthScore = computed(() => passwordStrengthMeta.value.score);
-    const passwordMinLengthMet = computed(() => passwordStrengthMeta.value.minLengthMet);
-    const passwordAlphaNumericMet = computed(() => passwordStrengthMeta.value.alphaNumericMet);
-    const passwordSpecialCharMet = computed(() => passwordStrengthMeta.value.specialCharMet);
-    const passwordStrengthLevel = computed(() => passwordStrengthMeta.value.level);
-
-    const passwordStrengthTextKey = computed(() => {
-
-      if (passwordStrengthLevel.value === 'weak') return 'auth.passwordStrengthWeak';
-
-      if (passwordStrengthLevel.value === 'medium') return 'auth.passwordStrengthMedium';
-
-      return 'auth.passwordStrengthStrong';
-
-    });
-
-    const passwordStrengthPercent = computed(() => passwordStrengthMeta.value.percent);
 
     const needCaptchaForEmailVerify = computed(() => {
 
@@ -2059,17 +1988,11 @@ export default {
 
       showPassword,
 
-      passwordMinLengthMet,
 
-      passwordAlphaNumericMet,
 
-      passwordSpecialCharMet,
 
-      passwordStrengthLevel,
 
-      passwordStrengthTextKey,
 
-      passwordStrengthPercent,
 
 
       config,
